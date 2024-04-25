8df11881dbd5857a80faee7e9112458e7b41f2a7
TccDs <166771474+PessoasDesaparecidas@users.noreply.github.com>
2024-04-25 11:52:54 -0300

    fix:arrumando tipo de relacionamento entre tabelas , usuarios e desaparecidos

--- bd.sql
+++ bd.sql
@@ -8,10 +8,11 @@ CREATE TABLE IF NOT EXISTS `contatos` (
 	`numero` INT(11) NOT NULL,
 	PRIMARY KEY (`id`)
 );
-CREATE TABLE IF NOT EXISTS `desaparecidos` (
+CREATE TABLE `desaparecidos` (
 	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
-	`contato_id` INT NOT NULL,
-	`nome` VARCHAR(100) NOT NULL,
+	`contato_id` INT ,
+	`user_id` INT ,
+    `nome` VARCHAR(100) NOT NULL,
 	`nome_social` VARCHAR(100) NOT NULL,
 	`idade` INT(3) NOT NULL,
 	`foto` BLOB NOT NULL,
@@ -20,31 +21,28 @@ CREATE TABLE IF NOT EXISTS `desaparecidos` (
 	`visto_por_ultimo` DATE NOT NULL,
 	`historia` VARCHAR(50) NOT NULL,
 	`regiao` VARCHAR(9) NOT NULL,
-	`esta_desaparecido` BOOLEAN NOT NULL DEFAULT true,
+	`esta_desaparecido` BOOLEAN   DEFAULT true,
 	created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
-	FOREIGN KEY (`contato_id`) REFERENCES `contatos`(`id`)
+	FOREIGN KEY (`contato_id`) REFERENCES `contatos`(`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `usuarios`(`id`)
 );
-CREATE TABLE IF NOT EXISTS `usuarios` (
+CREATE TABLE `usuarios` (
 	`id` INT AUTO_INCREMENT NOT NULL UNIQUE,
 	`nome` VARCHAR(100) NOT NULL,
 	`email` VARCHAR(50) NOT NULL UNIQUE,
 	`senha` VARCHAR(12)  NOT NULL,
 	`telefone` INT(11) NOT NULL,
-	`desaparecido_id` INT ,
 	created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	PRIMARY KEY (`id`),
-	FOREIGN KEY (`desaparecido_id`) REFERENCES `desaparecidos`(`id`)
+	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `banidos` (
 	`user_id` INT NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `usuarios`(`id`)
 );
-
-
 
 /*DELETANDO TABELA*/
 DROP TABLE `banidos`;
@@ -67,10 +65,13 @@ SELECT * FROM `contatos`;
 /*com seu relacionamento*/
 
 /*populando banco de dados*/
+/*usuarios*/
 INSERT INTO `usuarios`(`nome`,`email`,`senha`,`telefone`) VALUES('fulano','fulano@gmail.com','12345678',967603371);
 INSERT INTO `usuarios`(`nome`,`email`,`senha`,`telefone`) VALUES('ciclano','ciclano@gmail.com','12345678',967603372);
 INSERT INTO `usuarios`(`nome`,`email`,`senha`,`telefone`) VALUES('deutrano','deutrano@gmail.com','12345678',967603373);
-
+/*desaparecidos*/
+INSERT INTO `desaparecidos`(`id`,`contato_id`,`user_id`,`nome`,`nome_social`,`idade`,`foto`,`genero`,`caracteristica`,`visto_por_ultimo`,`historia`,`regiao`,`esta_desaparecido`) 
+VALUES(null,null,'1','henry','panko',17,'./assets/image.jpg','masculino','no tengo','2024-04-25 11:47:14','henry um jovem estudante da etec','norte',null);
 
 
 
@@ -85,3 +86,4 @@ ALTER TABLE `usuarios` ADD CONSTRAINT `usuarios_fk5` FOREIGN KEY (`desaparecido_
 ALTER TABLE `usuarios` ADD CONSTRAINT email UNIQUE (email);
 ALTER TABLE `desaparecidos` ADD CONSTRAINT `desaparecidos_fk1` FOREIGN KEY (`contato_id`) REFERENCES `contatos`(`id`);
 ALTER TABLE `banidos` ADD CONSTRAINT `banidos_fk0` FOREIGN KEY (`user_id`) REFERENCES `usuarios`(`id`);
+
