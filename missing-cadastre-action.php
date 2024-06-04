dc853228409bc2b35a394858a65a22aca85a5420
henry <franzhenry46.com>
2024-06-04 10:05:00 -0300

    feta:adicionando arquivo rf rnf rn , e tirando comentarios missing-cadastre-action.php

--- missing-cadastre-action.php
+++ missing-cadastre-action.php
@@ -1,6 +1,13 @@
 <?php
 session_start();
+
+$id_usuario = $_SESSION['id_user'] ? $_SESSION['id_user'] : "não temos id do usuario";
+echo  $id_usuario;
+
+
+
 if (isset($_POST['btn-cdastre-missing'])) {
+
   $nome_desaparecido = trim($_POST['nome_desaparecido']);
   $contato_desaparecido = trim($_POST['contato_desaparecido']);
   $observacao_desaparecido = trim($_POST['observacao_desaparecido']);
@@ -8,7 +15,6 @@ if (isset($_POST['btn-cdastre-missing'])) {
   $data_nascimento = trim($_POST['data_nascimento']);
   $local_desaparecimento = trim($_POST['local_desaparecimento']);
 
-  //Salvando a imagem 
   $extensao = strtolower(substr($_FILE['imagem']['name'], -4));
   $foto_desaparecido = md5(time()) . $extensao;
   $diretorio = "./assets/uploads/";
@@ -16,50 +22,12 @@ if (isset($_POST['btn-cdastre-missing'])) {
 
 
 
-  echo "<pre>";
+  echo "
+<pre>";
   $query = "INSERT INTO Desaparecido 
 (id_usuario, nome_desaparecido, foto_desaparecido, contato_desaparecido, observacao_desaparecido, data_desaparecimento ,data_nascimento, local_desaparecimento)
 VALUES
     (, '$nome_desaparecido', '$foto_desaparecido', '$contato_desaparecido', '$observacao_desaparecido','$data_desaparecimento', '$data_nascimento', '$local_desaparecimento')";
 }
-echo "</pre>";
-
-/*
 print_r($query);
-
-
-
-  echo "<br>";
-  echo "<br>";
-  echo "<br>";
-  echo "<br>";
-
-  echo " nome_desaparecido <br>";
-  print_r($nome_desaparecido);
-  echo "<br>";
-
-  echo " contato_desaparecido <br>";
-  print_r($contato_desaparecido);
-  echo "<br>";
-
-  echo " observacao_desaparecido <br>";
-  print_r($observacao_desaparecido);
-  echo "<br>";
-
-  echo " data_desaparecimento <br>";
-  print_r($data_desaparecimento);
-  echo "<br>";
-
-  echo " data_nascimento <br>";
-  print_r($data_nascimento);
-  echo "<br>";
-
-  echo " local_desaparecimento <br>";
-  print_r($local_desaparecimento);
-  echo "<br>";
-
-
-  echo "novo nome:";
-  print_r($foto_desaparecido);
-  echo "<br>";
-   */
+echo "</pre>";
---
+++ requests.md
@@ -0,0 +1,21 @@
+## REQUISITOS FUNCIONAIS
+
+[ X ] se cadastrar na plataforma
+[ X ] fazer login e logout na plataforma
+[ ] ver pessoas desaparecidas
+[ ] cadastrar pessoas desparecidas
+[ ] deletar pessoas desaparecidas
+[ ] atualizar pessoas desparecidas
+[ ] envio de email ao usuario
+
+## REGRAS DE NEGOCIO
+
+[ X ] não posso ter posso cadastrar usuario com email duplicado
+[ ] não posso cadastrar desaparecidos sem estar autenticado
+[ ] para o cadastro devo informar todas credenciais essenciais
+
+## REQUISITOS NÂO FUNCIONAIS 
+
+[ ] utilização doambiente de desenvolvimento AMP
+[ ] usabilidade
+[ ] segurança
