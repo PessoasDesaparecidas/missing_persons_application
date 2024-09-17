dafa5cd66712347798f9d31bd728bcca9c120097
pessoasDesaparecidas <etecpj.ds.2024@gmail.com>
2024-09-17 12:22:27 -0300

    arrumando erro de referencia em get-user-id

--- utils/get-user-id.php
+++ utils/get-user-id.php
@@ -2,10 +2,10 @@
 session_start();
 function get_user_id()
 {
-  $user_id = $_SESSION['user_id'];
+
+  $user_id = $_SESSION['user_id'] ?? false;
   if ($user_id) {
     return $user_id;
   }
-  $_SESSION['user_id'] = "";
   return false;
 }
