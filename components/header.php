883c93010119ce9829a0f0e4abf55f44d94f59c2
TccDs <166771474+PessoasDesaparecidas@users.noreply.github.com>
2024-11-25 11:56:00 -0300

    Update header.php

--- components/header.php
+++ components/header.php
@@ -5,7 +5,6 @@ $user = find_by_id($connection, get_user_id());
 <header id="header">
   <input type="checkbox" name="" id="chek">
   <label for="chek"><i class="fa-solid fa-bars"></i></label>
-<<<<<<< HEAD
   <h1 class="logo">
     <?php if ($leanguage == "pt"): ?>
     Busca Solidária
@@ -13,25 +12,9 @@ $user = find_by_id($connection, get_user_id());
     Búsqueda Solidaria
     <?php elseif ($leanguage == "en"): ?>
     Solidarity Search
-<<<<<<< HEAD
-    
-    <?php endif; ?>
-=======
     
     <?php endif; ?>
   </h1>
-=======
-  <h1 class="logo">
-    <?php if ($leanguage == "pt"): ?>
-    Busca Solidária
-    <?php elseif ($leanguage == "es"): ?>
-    Búsqueda Solidaria
-    <?php elseif ($leanguage == "en"): ?>
-    Solidarity Search
-    <? endif; ?>
->>>>>>> d9917f9db85c3cb07502f982dee3545f8861e675
-  </h1>
->>>>>>> 36149c0c5c4ce92e3be05ba69e3ccfe2ea6abead
   <nav>
 
     <?php if ($leanguage == "pt"): ?>
@@ -39,7 +22,6 @@ $user = find_by_id($connection, get_user_id());
     <a href="./orientacao.php">Orientações</a>
     <a href="./desaparecidos.php">Desaparecidos</a>
     <a href="#sobre">Sobre</a>
-<<<<<<< HEAD
     <?php elseif ($leanguage == "es"): ?>
     <a href="./index.php">Inicio</a>
     <a href="./orientacao.php">Orientaciones</a>
@@ -50,28 +32,9 @@ $user = find_by_id($connection, get_user_id());
     <a href="./orientacao.php">Guidelines</a>
     <a href="./desaparecidos.php">Missing</a>
     <a href="#sobre">About</a>
-<<<<<<< HEAD
-    
-    <?php endif; ?>
-=======
     
     <?php endif; ?>
 
-=======
-    <?php elseif ($leanguage == "es"): ?>
-    <a href="./index.php">Inicio</a>
-    <a href="./orientacao.php">Orientaciones</a>
-    <a href="./desaparecidos.php">Desaparecido</a>
-    <a href="#about">Acerca de</a>
-    <?php elseif ($leanguage == "en"): ?>
-    <a href="./index.php">Home</a>
-    <a href="./orientacao.php">Guidelines</a>
-    <a href="./desaparecidos.php">Missing</a>
-    <a href="#sobre">About</a>
-    <? endif; ?>
->>>>>>> d9917f9db85c3cb07502f982dee3545f8861e675
-
->>>>>>> 36149c0c5c4ce92e3be05ba69e3ccfe2ea6abead
   </nav>
 
   <!--começa drop de perfil-->
@@ -93,27 +56,13 @@ $user = find_by_id($connection, get_user_id());
           <i class="fa-regular fa-user"></i>
           <?php if ($leanguage == "pt"): ?>
           Editar perfil
-<<<<<<< HEAD
 
           <?php elseif ($leanguage == "es"): ?>
           Editar perfil
           <?php elseif ($leanguage == "en"): ?>
           Edit profile
-<<<<<<< HEAD
           
           <?php endif; ?>
-=======
-          
-          <?php endif; ?>
-=======
-
-          <?php elseif ($leanguage == "es"): ?>
-          Editar perfil
-          <?php elseif ($leanguage == "en"): ?>
-          Edit profile
-          <? endif; ?>
->>>>>>> 36149c0c5c4ce92e3be05ba69e3ccfe2ea6abead
->>>>>>> d9917f9db85c3cb07502f982dee3545f8861e675
         </a>
       </li>
 
@@ -122,25 +71,12 @@ $user = find_by_id($connection, get_user_id());
           <i class="fa-regular fa-circle-question"></i>
           <?php if ($leanguage == "pt"): ?>
           Ver seus desaparecidos
-<<<<<<< HEAD
           <?php elseif ($leanguage == "es"): ?>
           ver tu desaparecidos
           <?php elseif ($leanguage == "en"): ?>
           See your missing
-<<<<<<< HEAD
           
           <?php endif; ?>
-=======
-          
-          <?php endif; ?>
-=======
-          <?php elseif ($leanguage == "es"): ?>
-          ver tu desaparecidos
-          <?php elseif ($leanguage == "en"): ?>
-          See your missing
-          <? endif; ?>
->>>>>>> 36149c0c5c4ce92e3be05ba69e3ccfe2ea6abead
->>>>>>> d9917f9db85c3cb07502f982dee3545f8861e675
         </a>
       </li>
 
